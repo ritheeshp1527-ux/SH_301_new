b3bf5a87512b5ac2f92ebb9abce01e49b189793f
ritheeshp1527-ux <ritheeshp1527@gmail.com>
2026-09-19 06:39:31 +0530

    pre-phase-13e-stations

--- frontend/src/components/digital-twin/ChargingStation.tsx
+++ frontend/src/components/digital-twin/ChargingStation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { Station } from '@/types/system.types';
+import { ChargingStation as Team1Station } from './team1/ChargingStation';
 
 interface ChargingStationProps {
   station: Station;
@@ -9,22 +10,10 @@ interface ChargingStationProps {
 
 export const ChargingStation: React.FC<ChargingStationProps> = ({ station, position, onSelect }) => {
   return (
-    <group position={position} onClick={(e: any) => { e.stopPropagation(); onSelect(station.station_id, 'STATION'); }}>
-      {/* Base */}
-      <mesh castShadow receiveShadow position={[0, 0.5, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="#34495e" roughness={0.7} />
-      </mesh>
-      {/* Pillar */}
-      <mesh castShadow position={[0, 1.5, 0]}>
-        <boxGeometry args={[0.6, 1.5, 0.6]} />
-        <meshStandardMaterial color="#7f8c8d" />
-      </mesh>
-      {/* Screen/UI face */}
-      <mesh position={[0, 1.8, 0.31]}>
-        <planeGeometry args={[0.4, 0.6]} />
-        <meshStandardMaterial color="#000000" emissive="#1abc9c" emissiveIntensity={station.occupancy ? 0.5 : 0.1} />
-      </mesh>
-    </group>
+    <Team1Station 
+      station_id={station.station_id} 
+      position={position} 
+      onSelect={onSelect} 
+    />
   );
 };
